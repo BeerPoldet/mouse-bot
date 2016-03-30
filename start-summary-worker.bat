75945f9d7c4f954f8f4d15a7e79619ed6cc31895
BeerPoldet <beer@pupasoft.com>
2016-03-30 23:10:16 +0700

    clean class

--- start-summary-worker.bat
+++
@@ -1,7 +0,0 @@
-set META_FILE_NAME=meta.properties
-set META_FILE_DIR=test_ps3
-
-cd "C:\Users\Poldet\dev\workspaces\mobicat\mouse-bot\absg\com\absg"
-call groovyc *.groovy
-call groovy SummaryWorker.groovy %META_FILE_NAME% %META_FILE_DIR%
-timeout /t -1
