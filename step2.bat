3702c1d428d3be4c02f837570cdc78ea964ea139
BeerPoldet <beer@pupasoft.com>
2016-03-31 13:05:00 +0700

    fixes bat script

--- step2.bat
+++ step2.bat
@@ -3,6 +3,8 @@ set META_FILE_DIR=BKK4667-U21
 cd "C:\Users\Poldet\dev\workspaces\mobicat\mouse-bot\absg\com\absg"
 call groovyc *.groovy
 call groovy VerifyWorker.groovy %META_FILE_DIR%
-if %1 == "nopause" goto start
-pause
-:start
+if "%1" == "nopause" (
+	exit
+) else (
+	pause
+)
